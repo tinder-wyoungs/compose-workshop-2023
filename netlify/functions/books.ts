200e0ff86bb04911bfeb0ac6f7d2434b7690d769
Will Youngs <william.youngs@gotinder.com>
2023-10-18 15:24:52 -0700

    Adding a list of books to the home page

--- netlify/functions/books.ts
+++ netlify/functions/books.ts
@@ -1,3 +1,24 @@
-// import { Config, Context } from '@netlify/functions';
+import { Config, Context } from '@netlify/functions';
+import csv from 'csvtojson';
 
-export default async () => {};
+export const config: Config = {
+  method: 'GET',
+  path: '/api/books{/:id}?',
+};
+
+export default async (req: Request, context: Context) => {
+  const { id } = context.params;
+  console.log(`Looking up ${id || 'all books'}...`);
+  const { origin } = new URL(req.url);
+  const response = await fetch(`${origin}/books.csv`);
+  const csvContent = await response.text();
+  const books = await csv().fromString(csvContent);
+  if (id) {
+    const book = books.find(b => b.id === id);
+    if (!book) {
+      return new Response('Not found', { status: 404 });
+    }
+    return Response.json(book);
+  }
+  return Response.json(books);
+};
